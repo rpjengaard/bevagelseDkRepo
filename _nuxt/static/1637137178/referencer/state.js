734e94aead7c4c74785363429ae2e5d8f9506568
rpjengaard <rpj@limbo.works>
2021-11-17 12:07:52 +0100

    Updates

--- _nuxt/static/1637137178/referencer/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FbevagelseDkRepo\u002F_nuxt\u002Fstatic\u002F1637137178",layout:"default",error:a,serverRendered:true,routePath:"\u002Freferencer",config:{_app:{basePath:"\u002FbevagelseDkRepo\u002F",assetsPath:"\u002FbevagelseDkRepo\u002F_nuxt\u002F",cdnURL:a}}}}(null));
