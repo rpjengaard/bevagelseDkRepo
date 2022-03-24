e1ebe32d0802d4aff3ab1c4c10356b2d17455639
rpjengaard <rpj@limbo.works>
2022-03-24 16:52:36 +0100

    Updates

--- _nuxt/static/1648134186/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fydelser","\u002Fom-bevaegelse","\u002Ftemplate","\u002F","\u002Fcases","\u002Fkontakt","\u002Findex-old"]})
---
+++ _nuxt/static/1648137090/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcases","\u002Ftemplate","\u002Findex-old","\u002Fkontakt","\u002Fydelser","\u002Fom-bevaegelse"]})
